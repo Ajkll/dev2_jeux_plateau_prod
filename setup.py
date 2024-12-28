60a657d72a712e7801fd1a89349849ed5a2f4f13
ajkll <HE202326@students.ephec.be>
2024-12-28 10:52:42 +0100

    pypi correction5

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup, find_packages
 
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="dev_2",
     version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    install_requires=[],  # Utiliser requirements.txt si nécessaire
-    description="dev2_jeux_de_plateau",
+    install_requires=[],  
+    description="Bibliothèque Python pour des jeux de plateau",
+    long_description=long_description,  
+    long_description_content_type="text/markdown",  
     author="Alex",
     author_email="Alexisjacobs@protonmail.com",
     url="https://github.com/Ajkll/dev2_jeux_plateau_prod",
@@ -18,7 +24,7 @@ setup(
     python_requires=">=3.6",
     entry_points={
         "console_scripts": [
-            "dev2=app.main:main",  # Permet d'exécuter `dev2` dans le terminal
+            "dev2=app.main:main", 
         ]
     },
 )
