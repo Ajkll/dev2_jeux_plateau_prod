ef27557eee27238a456576a381f15ebd1667d8ba
PasRP-Theo <theomertens12345@gmail.com>
2025-01-02 10:05:49 +0100

    Update pion.py

--- src/module_perso/pion.py
+++ src/module_perso/pion.py
@@ -1,18 +1,15 @@
 class Pion:
-    def __init__(self, nom):
+    def init(self, nom):
         self.nom = nom
         self.position = 0
 
-    def deplacer(self, deplacement):
-        if deplacement < 0:
-            raise ValueError("Le nombre de pas doit être positif.")
-        self.position += deplacement
+    def deplacer(self, pas):
+        """bouge le pion de 'pas' case"""
+        self.position = max(0, self.position + pas)
 
-    def reculer(self, deplacement):
-        self.position = max(0, self.position - deplacement)
+    def reset(self):
+        """remet le pion sur la case 0"""
+        self.position = 0
 
-    def sur_case(self, case):
-        return self.position == case
-
-    def __str__(self):
+    def str(self):
         return f"{self.nom}, vous êtes à la position {self.position}."
